b3f4571c058c933c74d65c6f7ece07d7c8acf698
Felipe <felipejunqueira@yahoo.com>
2025-11-04 17:50:45 -0300

    Finalizado

--- app/page.js
+++ app/page.js
@@ -61,101 +61,102 @@ function Home() {
   ];
 
   return (
-    <div className="p-4 min-h-screen bg-[#f5efe6] ">
-      <div className="bg-gray-200 rounded-lg p-4 mb-25 mt-20">
-        <h2 className="text-6xl font-semibold text-gray-800 mb-9 text-center">
-          Bem-vinda ao <strong className="text-purple-600">Donas da Bola!</strong>
+    <div className="p-4 min-h-screen bg-[rgba(0,0,0,0.9)]">
+      <div className="rounded-lg p-4 mb-25 mt-20">
+        <h2 className="text-6xl font-semibold text-white mb-9 text-center">
+          Bem-vinda ao <strong className="text-purple-400">Donas da Bola!</strong>
         </h2>
-        <p className="text-2xl text-gray-600 leading-relaxed mb-2 text-center pl-15 pr-15">
+        <p className="text-2xl text-white/90 leading-relaxed mb-2 text-center pl-15 pr-15">
           Aqui o futebol é delas! Conecte-se com outras jogadoras, participe de eventos, inscreva-se em torneios, organize peladas semanais e viva o esporte com segurança e liberdade.
         </p>
-        <p className="text-2xl text-gray-600 leading-relaxed mb-2 text-center">
+        <p className="text-2xl text-white/90 leading-relaxed mb-2 text-center">
           Este é o seu espaço para brilhar, jogar e crescer dentro e fora das quadras. ⚽💜
         </p>
       </div>
       <div>
-        <h1 className="text-center font-semibold p-4 text-white bg-purple-600 ml-200 mr-200 text-3xl border border-black rounded-lg">Eventos</h1>
+          <h1 className="text-center font-semibold p-4 text-white bg-purple-600 ml-200 mr-200 text-3xl border border-black rounded-lg  mb-5">Eventos</h1>
       </div>
-      <div className="mb-3 bg-[rgba(0,0,0,0.9)] p-2 pb-8 rounded-lg mt-5">
-        <h3 className="text-2xl font-semibold text-white mb-4 mt-5 ml-5">Copinha de Futebol</h3>
-        
-        <div className="grid grid-cols-2 gap-3 mb-4 p-2">
-          <div className="bg-white border border-purple-600 rounded-lg p-3 relative">
-            <h4 className="font-medium text-gray-800 mb-2">Copa Sul</h4>
-            <div className="flex items-center text-sm text-gray-600 mb-1">
-              <Calendar className="w-4 h-4 mr-1" />
-              <span>17 JUL, 2025</span>
+      <div className="rounded-lg p-3 bg-[rgba(0,0,0,)] border border-purple-600">
+         
+        <div className="mb-3  p-2 pb-8 rounded-lg mt-5">
+          <h3 className="text-2xl font-semibold text-white mb-4 mt-5 ml-5">Copinha de Futebol</h3>
+          
+          <div className="grid grid-cols-2 gap-3 mb-4 p-2">
+            <div className="bg-white border border-purple-600 rounded-lg p-3 relative">
+              <h4 className="font-medium text-gray-800 mb-2">Copa Sul</h4>
+              <div className="flex items-center text-sm text-gray-600 mb-1">
+                <Calendar className="w-4 h-4 mr-1" />
+                <span>17 JUL, 2025</span>
+              </div>
+              <div className="flex items-center text-sm text-gray-600">
+                <Clock className="w-4 h-4 mr-1" />
+                <span>18:00</span>
+              </div>
+              <div className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-green-500 p-1 rounded-md text-center text-white w-32">
+                <Link
+                  href="#"
+                  className="w-full hover:opacity-80 cursor-pointer block text-white"
+                >
+                  Mais informações
+                </Link>
+              </div>
             </div>
-            <div className="flex items-center text-sm text-gray-600">
-              <Clock className="w-4 h-4 mr-1" />
-              <span>18:00</span>
-            </div>
-            <div className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-green-500 p-1 rounded-md text-center text-white w-32">
-              <Link
-                href="#"
-                className="w-full hover:opacity-80 cursor-pointer block text-white"
-              >
-                Mais informações
-              </Link>
+
+            <div className="bg-white border border-purple-600 rounded-lg p-3 relative">
+              <h4 className="font-medium text-gray-800 mb-2">Copa São Paulo</h4>
+              <div className="flex items-center text-sm text-gray-600 mb-1">
+                <Calendar className="w-4 h-4 mr-1" />
+                <span>17 JUL, 2025</span>
+              </div>
+              <div className="flex items-center text-sm text-gray-600">
+                <Clock className="w-4 h-4 mr-1" />
+                <span>14:00</span>
+              </div>
+              <div className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-green-500 p-1 rounded-md text-center text-white w-32">
+                <Link
+                  href="#"
+                  className="w-full hover:opacity-80 cursor-pointer block text-white"
+                >
+                  Mais informações
+                </Link>
+              </div>
             </div>
           </div>
-
-          <div className="bg-white border border-purple-600 rounded-lg p-3 relative">
-            <h4 className="font-medium text-gray-800 mb-2">Copa São Paulo</h4>
-            <div className="flex items-center text-sm text-gray-600 mb-1">
-              <Calendar className="w-4 h-4 mr-1" />
-              <span>17 JUL, 2025</span>
+          <h1 className="text-2xl font-semibold text-white mb-4 mt-25 text-center ">
+            Evento corporativo
+          </h1>
+          <div className="bg-purple-600 text-white rounded-lg p-4 mb-10 ml-3 mr-3 mt-2 relative">
+          <div className="flex items-center justify-between">
+            <div className="flex-1">
+              <h3 className="font-semibold mb-1">Futebol Semanal - Gratuito</h3>
+              <div className="flex items-center text-sm mb-1">
+                <Calendar className="w-4 h-4 mr-1" />
+                <span>Rio de Janeiro, Terça-feira 17</span>
+              </div>
+              <p className="text-sm">Inscrições fecham às 14:15</p>
             </div>
-            <div className="flex items-center text-sm text-gray-600">
-              <Clock className="w-4 h-4 mr-1" />
-              <span>14:00</span>
-            </div>
-            <div className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-green-500 p-1 rounded-md text-center text-white w-32">
-              <Link
-                href="#"
-                className="w-full hover:opacity-80 cursor-pointer block text-white"
-              >
-                Mais informações
-              </Link>
-            </div>
+            <Link
+                  href="#"
+                  className=" hover:opacity-80 cursor-pointer block text-white bg-green-500 p-1 rounded-md text-center w-32"
+                >
+                  Se inscreva Aqui!
+            </Link>
           </div>
         </div>
-        <h1 className="text-2xl font-semibold text-white mb-4 mt-25 text-center ">
-          Evento corporativo
-        </h1>
-        <div className="bg-purple-600 text-white rounded-lg p-4 mb-10 ml-3 mr-3 mt-2 relative">
-        <div className="flex items-center justify-between">
-          <div className="flex-1">
-            <h3 className="font-semibold mb-1">Futebol Semanal - Gratuito</h3>
-            <div className="flex items-center text-sm mb-1">
-              <Calendar className="w-4 h-4 mr-1" />
-              <span>Rio de Janeiro, Terça-feira 17</span>
-            </div>
-            <p className="text-sm">Inscrições fecham às 14:15</p>
-          </div>
-          <Link
-                href="#"
-                className=" hover:opacity-80 cursor-pointer block text-white bg-green-500 p-1 rounded-md text-center w-32"
-              >
-                Se inscreva Aqui!
-          </Link>
         </div>
-      </div>
-      </div>
-
-      <div>
-        <h1 className="text-center font-semibold p-4 text-white bg-purple-600 mt-20 mb-5 ml-200 mr-200 text-3xl border border-black rounded-lg">Próximos Jogos</h1>
-      </div>
-      <div className="bg-[rgba(0,0,0,0.9)] p-8 rounded-lg">
-      {matches.map((match, index) => (
-        <MatchCard key={index} {...match} />
-      ))}
-    </div>
-
-      <CadastroJogadora 
-        isOpen={isCadastroOpen} 
-        onClose={handleCloseCadastro} 
-      />
+        </div>
+        <div className="">
+          <h1 className="text-center font-semibold p-4 text-white bg-purple-600 mt-20 mb-5 ml-200 mr-200 text-3xl border border-black rounded-lg">Próximos Jogos</h1>
+        </div>
+        <div className="">
+          {matches.map((match, index) => (
+            <MatchCard key={index} {...match} />
+          ))}
+         </div>      
+          <CadastroJogadora 
+            isOpen={isCadastroOpen} 
+            onClose={handleCloseCadastro} 
+          />
     </div>
   );
 }
